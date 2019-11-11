40e48c5d5d534259bedb3347396eaa9d509382b2
Brandon Mathis <brandon@imathis.com>
2019-11-11 09:43:16 -0600

    Fixed element block test

--- test/spark/element_test.rb
+++ test/spark/element_test.rb
@@ -4,12 +4,11 @@ require "test_helper"
 
 module Spark
   class ElementIntegrationTest < ActionDispatch::IntegrationTest
-    parallelize_me!
 
     def test_render_element
       get "/element/block"
       assert_response :success
-      assert_equal trim_result(response.body), "<div>content</div>"
+      assert_equal trim_result(response.body), "<div><span>content</span></div>"
     end
   end
 end
